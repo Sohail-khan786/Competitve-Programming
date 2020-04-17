c02ac6cf097ecb804926e7abea1ae8b0156e83d0
Sohail Khan <Khansohail0540@gmail.com>
2020-04-18 03:47:39 +0530

    template added

---
+++ template.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+//Omit before Submitting the Code
+#ifndef ONLINE_JUDGE
+    freopen("input.txt" , "r" , stdin);
+    freopen("output.txt" ,"w" , stdout);
+#endif
+
+
+	int x;
+	cin>>x;
+	cout<<x+1<<endl;
+
+	cout<<"hello"<<endl;
+	return 0;
+}
